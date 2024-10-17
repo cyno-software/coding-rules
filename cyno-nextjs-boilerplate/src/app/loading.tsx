f47bcf4084e95e24a38041411ef93240b8e8f3fc
hieuthahn <hieuthahn@gmail.com>
2024-10-17 22:49:39 +0700

    Add global handle page

---
+++ cyno-nextjs-boilerplate/src/app/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Loading() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="size-32 animate-spin rounded-full border-y-2 border-gray-900" />
+    </div>
+  )
+}
