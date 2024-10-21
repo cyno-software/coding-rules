267dbc9ed856c47092ff793bd2922b75d390aa62
hieuthahn <hieuthahn@gmail.com>
2024-10-21 16:27:21 +0700

    Add `UploadInput` component

--- cyno-nextjs-boilerplate/src/app/samples/page.tsx
+++ cyno-nextjs-boilerplate/src/app/samples/page.tsx
@@ -3,6 +3,9 @@ import React, {
 } from "react"
 
 import {
+  UploadInput,
+} from "~/components/inputs/upload-input"
+import {
   EmployeesTable,
 } from "~/components/tables/employees-table"
 
@@ -10,6 +13,19 @@ export default function SampleEmployeesPage(): React.ReactElement {
   return (
     <Suspense>
       <div className="container mx-auto my-4">
+        <UploadInput
+          apiUrl="https://api.example.com"
+          authToken="your-auth-token"
+          onServerUploaded={
+            (response) => {
+              // TODO: Handle response
+              // eslint-disable-next-line no-console
+              console.log(
+                "Tải lên hoàn tất:", response
+              )
+            }
+          }
+        />
         <EmployeesTable />
       </div>
     </Suspense>
